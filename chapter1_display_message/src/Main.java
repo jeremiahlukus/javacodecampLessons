4ecc19ba1b27bc91404d463bdb7e2cc1a4b4ac11
jeremiahlukus <jeremiahlukus1@gmail.com>
2018-06-18 12:12:53 -0400

    Chapter one

---
+++ chapter1_display_message/src/Main.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate current_date = java.time.LocalDate.now();
+        System.out.println("I, Jeremiah Parrack, am determined to learn how to code.");
+        System.out.println("Today's date is: " + current_date);
+    }
+}
